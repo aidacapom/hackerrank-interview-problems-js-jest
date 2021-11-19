0c2a2fd3914b723795e7d34e5f7267ec3295b29c
Aida Capó Martí <aidacapom@gmail.com>
2021-11-19 15:27:55 +0100

    Rotate Array Left Challenge done and tested

--- src/js/arrays/rotate-array-left.js
+++ src/js/arrays/rotate-array-left.js
@@ -1,5 +1,9 @@
 export default function rotateArrayToLeft(a, d) {
+    if(d < 0) {
+        throw new Error('Introduce a positive number of rotations')
+    }
+    
     const headIndex = d % a.length
-    const head = a.splice(0, headIndex)
-    return a.concat(head)
+    const head = a.splice(0, headIndex) //head contains the first headIndex elements of the array, which are removed from the array.
+    return a.concat(head) //concat() returns a new array, containing the joined arrays; it does not change the existing arrays.
 }
--- src/js/arrays/rotate-array-left.test.js
+++ src/js/arrays/rotate-array-left.test.js
@@ -1,7 +1,22 @@
 import rotateArrayToLeft from "./rotate-array-left"
 
 describe('Left Rotation Operation on an Array', () => {
-    it('', () => {
+    it('works when the number of rotations is bigger than the array length', () => {
+        expect(rotateArrayToLeft([1, 2, 3], 4)).toEqual([2, 3, 1])
+    })
 
+    it('works when the number of rotations is smaller than the array length', () => {
+        expect(rotateArrayToLeft([1, 2, 3], 1)).toEqual([2, 3, 1])
+    })
+
+    it('negative number of rotations throws an error', () => {
+        /* La función que arroja una excepción necesita ser invocada dentro de una función de envoltorio, 
+         * de lo contrario la afirmación toThrow fallará
+         */
+        expect(() => rotateArrayToLeft([1, 2, 3], -1)).toThrow('Introduce a positive number of rotations')
+    })
+
+    it('0 rotations return the given array', () => {
+        expect(rotateArrayToLeft([1, 2, 3], 0)).toEqual([1, 2, 3])
     })
 })
